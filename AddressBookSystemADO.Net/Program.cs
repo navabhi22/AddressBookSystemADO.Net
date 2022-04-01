755245b3d000c0647fb4e51bbcf5e3f4cb878a89
navabhi22 <navabhi22@gmail.com>
2022-04-01 22:28:14 +0530

    Greeting message fot  Address Book System Program

---
+++ AddressBookSystemADO.Net/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AddressBookSystemADO.Net
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome TO AddressBook Problems Using ADO.NET");
+        }
+    }
+}
